15c6bded5216f5b09a6ab31d60ff28b1ee2e4684
Alenka <ms.alenchik@gmail.com>
2020-11-23 20:20:57 +0200

    Fixed mistake

--- 11.2.js
+++ 11.2.js
@@ -5,6 +5,8 @@ function isPrime(num) {
         else if (num <= 1000 && num % i !== 0 && num !== 0) { return `${num} - простое число!` }
         else if (num > 1000 && num !== 0) { return `Данные не верны.` }
     }
+    if (num === 1) { return `1 - не является простым числом` }
+    else if (num === 0) {return `Введите число больше 0.`}
 }
     
 console.log (isPrime (5))
