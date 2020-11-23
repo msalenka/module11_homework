162c67abaf1743e2597c76ce7aaf3d84e6bb6cbc
Alenka <ms.alenchik@gmail.com>
2020-11-23 20:11:28 +0200

    homework 11.2

--- 11.2.js
+++ 11.2.js
@@ -1,3 +1,14 @@
-function primeNumber () {
-for (let i=0; i <= 1000; i++) {
-if (
+function isPrime(num) {
+    for (let i = 2; i < num; i++) {
+        if (num <= 1000 && num % i === 0 && num !== 0) { return `${num} - не является простым числом!` }
+
+        else if (num <= 1000 && num % i !== 0 && num !== 0) { return `${num} - простое число!` }
+        else if (num > 1000 && num !== 0) { return `Данные не верны.` }
+    }
+}
+    
+console.log (isPrime (5))
+
+    
+
+
