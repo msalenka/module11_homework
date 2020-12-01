70f987760d10b74fdca7673692872d53749af755
Alenka <ms.alenchik@gmail.com>
2020-12-01 19:53:17 +0200

    fixed mistake

--- 11.2.js
+++ 11.2.js
@@ -1,7 +1,6 @@
 function isPrime(num) {
     for (let i = 2; i < num; i++) {
         if (num <= 1000 && num % i === 0 && num !== 0) { return `${num} - не является простым числом!` }
-
         else if (num <= 1000 && num % i !== 0 && num !== 0) { return `${num} - простое число!` }
         else if (num > 1000 && num !== 0) { return `Данные не верны.` }
     }
